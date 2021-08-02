63e53de445ea5063ae451568c0065e82a648ac25
Jansen-Stanlie <jansenstan24@gmail.com>
2021-08-02 15:45:09 +0700

    Router

--- src/App.js
+++ src/App.js
@@ -1,46 +1,41 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import {BrowserRouter as Router} from "react-router-dom";
 import "./App.css";
-import { Navbar, Body, Footer } from "./template";
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentPage: "home",
-      loginStatus: true,
-    };
-  }
-  changePage = (page) => {
-    this.setState({
-      currentPage: page,
-    });
-  };
-  changeStatus = (status, page) => {
-    this.setState({
-      currentPage: page,
-      loginStatus: status,
-    });
-  };
-  render() {
-    console.log(this.state.currentPage);
-    return (
-      <>
-        <Navbar
-          page={this.state.currentPage}
-          goToPage={this.changePage}
-          loginStatus={this.state.loginStatus}
-          changeStatus={this.changeStatus}
-        />
+import {Navbar, Body, Footer} from "./template";
 
-        <Body
-          page={this.state.currentPage}
-          goToPage={this.changePage}
-          changeStatus={this.changeStatus}
-          loginStatus={this.state.loginStatus}
-        />
-        <Footer />
-      </>
-    );
-  }
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loginStatus: true,
+        };
+    }
+    changeStatus = (status, page) => {
+        this.setState({
+            currentPage: page,
+            loginStatus: status,
+        });
+    };
+
+    render() {
+        console.log(this.state.currentPage);
+        return (
+            <>
+              <Router>
+                <Navbar
+                    loginStatus={this.state.loginStatus}
+                    changeStatus={this.changeStatus}
+                />
+
+                <Body
+                    changeStatus={this.changeStatus}
+                    loginStatus={this.state.loginStatus}
+                />
+                <Footer/>
+              </Router>
+            </>
+        );
+    }
 }
 
 export default App;
